cffa0acd66907877af4e2cc8fed768caed45911b
tancos02 <37735433+tancos02@users.noreply.github.com>
2019-03-31 20:46:10 +0700

    Add files via upload

--- trial.py
+++ trial.py
@@ -13,40 +13,40 @@ def PathMaker(node) :
         node = node[2] 
     return(path)
 
-def BFS(mat,x,y) :
-    queue = deque([(x,y,None)])
-    while (len(queue) > 0) :
-        node = queue.popleft()
-        x = node[0]
-        y = node[1]
-        if(mat[x][y] == '3') :
-            return (PathMaker(node))
-        if(mat[x][y] != '0') :
-            continue
-        mat[x][y] == '2'
-        for i in [[x-1,y],[x+1,y],[x,y-1],[x,y+1]] :
-            queue.append((i[0],i[1],node))
-    return []
+def BFS(x,y,Map): 
+    queue = deque( [(x,y,None)]) #create queue 
+    while len(queue)>0: #make sure there are nodes to check left 
+        node = queue.popleft() #grab the first node 
+        x = node[0] #get x and y 
+        y = node[1] 
+        if Map[x][y] == "3": #check if it's an exit 
+            return PathMaker(node) #if it is then return the path 
+        if (Map[x][y]!="0"): #if it's not a path, we can't try this spot 
+            continue 
+        Map[x][y]="2" #make this spot explored so we don't try again 
+        for i in [[x-1,y],[x+1,y],[x,y-1],[x,y+1]]: #new spots to try 
+            queue.append((i[0],i[1],node))#create the new spot, with node as the parent 
+    return [] 
 
 def MapDrawBFS(mat,path) :
     for x in range(0,len(mat)): 
         for y in range(0,len(mat[x])): 
             if ((x,y) in path): 
-                assert mat[x][y] in ('0','3') 
-                print("-",end="") 
+                assert mat[x][y] in ("2","3") 
+                print("-",end=" ") 
             elif (mat[x][y]=='1'): 
-                print("+",end="") 
+                print("+",end=" ") 
             elif (mat[x][y]=='3'): 
-                print("*",end="") 
+                print("*",end=" ") 
             else: 
-                print(' ',end="") 
+                print(' ',end=" ") 
         print() 
 
 def main() :
     fileName = "input.txt"
     mat = matrixReader(fileName)
     print("Input Maze : ")
-    print(mat)
+    MapDrawBFS(mat,[])
     print("Starting Point : ")
     start = np.zeros((2,),dtype = int)
     start[0] = int(input())
@@ -55,8 +55,7 @@ def main() :
     finish = np.zeros((2,),dtype = int)
     finish[0] = int(input())
     finish[1] = int(input())
-    mat[finish[0]][finish[1]] = "3"
-    print(mat)
-    MapDrawBFS(mat,BFS(mat,start[0],start[1]))
+    mat[finish[0]][finish[1]] = '3'
+    MapDrawBFS(mat,BFS(start[0],start[1],mat))
 
 main()
